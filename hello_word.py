109f24f54dd2f61217653f128004728a1395cec2
semibj1234-debug <semibj1234@gmail.com>
2025-09-09 12:49:14 +0900

    ""Second commit"

--- hello_word.py
+++ hello_word.py
@@ -1,5 +1,10 @@
 def printf_hello():
 	print("Hello world")
 
+def print_KIT():
+	print("Kumoh National Insitute of Technology")
+
 if __name__ == '_main_' :
+	print_hello
+	print_KIT()
 	print_hello
